8726f1492f7d6c3301aac0117776c777bdbfec4b
kokush <witalik.logoyda@gmail.com>
2024-06-04 20:25:03 +0300

    change cors for prod

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const CORS = {
-	origin: "http://localhost:5173", // Specify the allowed origin
+	origin: "https://kokush-v.github.io/incode", // Specify the allowed origin
 	methods: ["GET", "POST", "PUT", "DELETE"], // Specify the allowed HTTP methods
 	allowedHeaders: ["Content-Type", "Authorization"], // Specify the allowed headers
 	exposedHeaders: ["Authorization"], // Specify the headers exposed to the client
